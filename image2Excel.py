0ad7d3edd18f2e38c136a038ae57b8d526f5c248
John Angelis <john_agelis@hotmail.com>
2018-09-08 19:30:34 +0300

    Fix color conditional formatting

--- image2Excel.py
+++ image2Excel.py
@@ -3,6 +3,10 @@ import numpy as np
 import xlsxwriter
 
 
+def rgb2hex(r, g, b):
+    return "#{:02x}{:02x}{:02x}".format(r, g, b)
+
+
 FILENAME = 'image.jpg'  # image can be in gif jpeg or png format
 image = Image.open(FILENAME).convert('RGB')
 
@@ -26,28 +30,31 @@ for j in range(height):
 file.close()
 
 
-workbook = xlsxwriter.Workbook('test1.xlsx')
+workbook = xlsxwriter.Workbook('image_ex.xlsx')
 worksheet = workbook.add_worksheet()
 
 pix = np.array(image2)
 
 
-def rgb2hex(r, g, b):
-    return "#{:02x}{:02x}{:02x}".format(r, g, b)
-
-
 pix = pix.transpose(0, 2, 1).reshape(-1, pix.shape[1])
+
+list_red = np.arange(0, pix.shape[0], 3)
+list_green = np.arange(1, pix.shape[0]-1, 3)
+# list_blue = np.arange(2, pix.shape[0]-2, 3)
 
 for row in range(0, pix.shape[0]):
     for col in range(pix.shape[1]):
         worksheet.write(row, col, pix[row, col])
-        # format1 = workbook.add_format({'bg_color': rgb2hex})
-        # worksheet.conditional_format(row, col, row, col, {'type': 'cell',
-        #                                                   'criteria': '==',
-        #                                                   'value': pix[row, col],
-        #                                                   'format': format1})
+        if row in list_red:
+            col_hex = rgb2hex(pix[row, col], 0, 0)
+        elif row in list_green:
+            col_hex = rgb2hex(0, pix[row, col], 0)
+        else:
+            col_hex = rgb2hex(0, 0, pix[row, col])
 
-
+        format1 = workbook.add_format({'bg_color': col_hex})
+        worksheet.conditional_format(row, 0, row, 100, {'type': 'cell',
+                                                        'criteria': '==',
+                                                        'value': pix[row, col],
+                                                        'format': format1})
 workbook.close()
-
-
