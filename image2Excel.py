7c83f32729f6c480c3153c055e3c407b34777234
John Angelis <john_agelis@hotmail.com>
2018-09-08 20:17:05 +0300

    Fixed Aspect ratio

--- image2Excel.py
+++ image2Excel.py
@@ -10,21 +10,19 @@ def rgb2hex(r, g, b):
 FILENAME = 'image.jpg'  # image can be in gif jpeg or png format
 image = Image.open(FILENAME).convert('RGB')
 
-width = image.size[0]
-height = image.size[1]
-# print(image.size)
-size = 150, 128  # this size works good with excel
+width, height = image.size
+aspect_ratio = width/height
+resized_height = 150
+resized_width = int(round(resized_height*aspect_ratio))
+size = resized_width, resized_height
 image2 = image.resize(size, Image.ANTIALIAS)
 image2.save('resize.jpg')
-width = image2.size[0]
-height = image2.size[1]
-print(image2.size)
 pixels = image2.load()
 
 file = open('output.txt', 'w')  # the file with the RGB values
-for j in range(height):
+for j in range(resized_height):
     for t in range(0, 3):
-        for i in range(width):
+        for i in range(resized_width):
             file.write('%d  \t' % pixels[i, j][t])
         file.write(' \n')
 file.close()
@@ -53,8 +51,8 @@ for row in range(0, pix.shape[0]):
             col_hex = rgb2hex(0, 0, pix[row, col])
 
         format1 = workbook.add_format({'bg_color': col_hex})
-        worksheet.conditional_format(row, 0, row, 100, {'type': 'cell',
-                                                        'criteria': '==',
-                                                        'value': pix[row, col],
-                                                        'format': format1})
+        worksheet.conditional_format(row, 0, row, pix.shape[1], {'type': 'cell',
+                                                                 'criteria': '==',
+                                                                 'value': pix[row, col],
+                                                                 'format': format1})
 workbook.close()
